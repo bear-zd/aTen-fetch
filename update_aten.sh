29c35c76305a4e5dc2e5f5512d284116e9ca0c1a
Zidea <58643709+bear-zd@users.noreply.github.com>
2024-10-10 16:47:34 +0800

    Update update_aten.sh

--- update_aten.sh
+++ update_aten.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 PYTORCH_REPO="https://github.com/pytorch/pytorch.git"
-TARGET_DIR="aten-src-ATen"
-rm -rf $TARGET_DIR
+TARGET_DIR_ATEN="aten-src-ATen"
+TARGET_DIR_C10="aten-src-c10"
+rm -rf $TARGET_DIR_ATEN
+rm -rf $TARGET_DIR_C10
 git clone --depth 1 $PYTORCH_REPO temp-pytorch
 mkdir -p $TARGET_DIR
-cp -r temp-pytorch/aten/src/ATen/* $TARGET_DIR/
-cp -r temp-pytorch/aten/src/c10 $TARGET_DIR/
-cp temp-pytorch/LICENSE $TARGET_DIR/
+cp -r temp-pytorch/aten/src/ATen/* $TARGET_DIR_ATEN/
+cp -r temp-pytorch/aten/src/c10/* $TARGET_DIR_C10/
+cp temp-pytorch/LICENSE .
 rm -rf temp-pytorch
 echo "Update complete."
