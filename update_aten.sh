8b378d4376f1dfd55c035e51eaf0191e0d7cc339
Zidea <58643709+bear-zd@users.noreply.github.com>
2024-10-10 16:49:01 +0800

    Update update_aten.sh

--- update_aten.sh
+++ update_aten.sh
@@ -5,7 +5,8 @@ TARGET_DIR_C10="aten-src-c10"
 rm -rf $TARGET_DIR_ATEN
 rm -rf $TARGET_DIR_C10
 git clone --depth 1 $PYTORCH_REPO temp-pytorch
-mkdir -p $TARGET_DIR
+mkdir -p $TARGET_DIR_ATEN
+mkdir -p $TARGET_DIR_C10
 cp -r temp-pytorch/aten/src/ATen/* $TARGET_DIR_ATEN/
 cp -r temp-pytorch/aten/src/c10/* $TARGET_DIR_C10/
 cp temp-pytorch/LICENSE .
